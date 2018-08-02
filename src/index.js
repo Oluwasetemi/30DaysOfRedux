f37b910a374f6d68ac8a7a1bc46c8b89d45a3821
Ojo Oluwasetemi <Oluwasetemi@users.noreply.github.com>
2018-08-02 10:19:38 +0100

    Day2
    Get State from the Redux Store

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ function App() {
     <div className="App">
       <h1>Hello DevCirclers!</h1>
       <h3>Just Follow the instructions on the Facebook Group </h3>
+      <label><input type="checkbox" name="days" checked />  Create a Redux Store </label>
+      <label><input type="checkbox" name="days" />  Get State from the Redux Store</label>
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,10 +1,5 @@
 import Redux from 'redux'
 
-const reducer = (state = 5) => {
-  return state
-}
+const store = Redux.createStore((state = 5) => state)
 
-// Redux methods are available from a Redux object
-// For example: Redux.createStore()
-// Define the store here:
-const store = Redux.createStore(reducer)
+const currentState = store.getState()
